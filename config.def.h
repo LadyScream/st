edb11f45578bb5057885c6b8b2ee0307089565dc
ladyscream <the.lady.scream@gmail.com>
2019-05-28 07:19:21 -0500

    Changed colorscheme

--- config.def.h
+++ config.def.h
@@ -85,30 +85,33 @@ unsigned int tabspaces = 8;
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
 	/* 8 normal colors */
-	"black",
-	"red3",
-	"green3",
-	"yellow3",
-	"blue2",
-	"magenta3",
-	"cyan3",
-	"gray90",
+	"#101010", /* black   */
+	"#e84f4f", /* red     */
+	"#b8d68c", /* green   */
+	"#e1aa5d", /* yellow  */
+	"#7dc1cf", /* blue    */
+	"#9b64fb", /* magenta */
+	"#6d878d", /* cyan    */
+	"#dddddd", /* white   */
 
 	/* 8 bright colors */
-	"gray50",
-	"red",
-	"green",
-	"yellow",
-	"#5c5cff",
-	"magenta",
-	"cyan",
-	"white",
+	"#404040", /* black   */
+	"#d23d3d", /* red     */
+	"#a0cf5d", /* green   */
+	"#f39d21", /* yellow  */
+	"#4e9fb1", /* blue    */
+	"#8542ff", /* magenta */
+	"#42717b", /* cyan    */
+	"#dddddd", /* white   */
 
+	/* special colors */
 	[255] = 0,
+	[256] = "#151515", /* background */
+	[257] = "#d7d0c7", /* foreground */
 
 	/* more colors can be added after 255 to use with DefaultXX */
-	"#cccccc",
-	"#555555",
+	/*"#cccccc",
+	"#555555",*/
 };
 
 
