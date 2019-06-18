9648338a7dd97651ba1aa51902e112682867035d
ladyscream <the.lady.scream@gmail.com>
2019-06-18 07:06:27 -0500

    Changed fonts

--- config.def.h
+++ config.def.h
@@ -5,10 +5,10 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Fira Mono:size=10:antialias=true:autohint=true";
+static char *font = "Fira Mono:size=11:antialias=true:autohint=true";
 /* Spare fonts */
 static char *font2[] = {
-	"-wuncon-siji-medium-r-normal--10-100-75-75-c-80-iso10646-1"
+	"mononoki Nerd Font:size=11:antialias=false:autohint=true"
 /*	"Inconsolata for Powerline:pixelsize=12:antialias=true:autohint=true", */
 /*	"Hack Nerd Font Mono:pixelsize=11:antialias=true:autohint=true", */
 };
